39394cbe57d46b325c8aad27c46eb21ae0e71007
Abdelrahman Reisha <77059381+Reisha76@users.noreply.github.com>
2024-08-10 14:30:02 +0300

    Update bank.h
    Added features to the project (find in README)

--- bank.h
+++ bank.h
@@ -6,6 +6,8 @@
 #include <iomanip>
 using namespace std;
 
+//enum Permissions {FullPermissions=-1};
+
 struct stClientData
 {
     string AccountNumber;
@@ -16,17 +18,37 @@ struct stClientData
     bool MarkClientForDeletion = false;
 };
 
+struct UserData     //new struct to hold user login info and perms
+{
+    string UserName;
+    string Password;
+    short UserPermissions;
+    bool MarkUserForDeletion = false;
+};
+
 const string FileName = "Clients.txt";
+const string FileNameUser = "Users.txt";
 const string delim = "#//#";
 
 enum enMenuChoices
 {
-    ShowClients = 1, AddClient = 2, DeleteClient = 3, UpdateClientInfo = 4, FindClient = 5, Transactions = 6, ExitMenu=7
+    ShowClients = 1, AddClient = 2, DeleteClient = 3, UpdateClientInfo = 4, FindClient = 5, Transactions = 6, ManageUsers=7, LogOut=8
 };
 
 enum enTransactionMenuChoices
 {
     Deposit=1, Withdraw=2, TotalBalance=3, MainMenu=4
+};
+
+enum enUserMenuChoices
+{
+    ListUsers=1, AddUser=2, DeleteUser=3, UpdateUser=4, FindUser=5, BackToMainMenu=6
+};
+
+enum enPermissions
+{
+    eAll = -1, pListClients = 1, pAddNewClient=2, pDeleteClient=4, pUpdateClient=8,
+    pFindClient=16, pTransactions=32, pManageUsers=64
 };
 
 void PrintAllClientsData(vector <stClientData> vClients);
@@ -70,3 +92,36 @@ short GetTransMenuChoice();
 void PrintBalancesList(vector <stClientData> vClients);
 void PrintBalancesRecords(stClientData Client);
 void ShowTotalBalance();
+/////////////////BANK EXTENSION//////////////////////
+void LoginScreen();
+bool CheckUserAndPass(string UserName, string Password, UserData& CurrentUser);
+vector <UserData> LoadUsersDataIntoVector();
+UserData ConvertUserLineToRecord(string Line, string delim);
+void StartProgram();
+void ShowManageUsersMenu();
+short GetUserMenuChoice();
+void ExecuteUsersMenuChoice(enUserMenuChoices UserMenuChoice);
+void PrintUserList(vector <UserData> vUsers);
+void PrintUserRecord(UserData User);
+void ReturnToUsersMenu();
+void PrintUserRecord(UserData User);
+void ReturnToUsersMenu();
+void FindUsersScreen();
+string GetUserNameToSearch();
+bool FindUserByUserName(string UserName, vector <UserData> vUsers, UserData& User);
+void PrintUserCard(UserData User);
+void AddingUsersScreen();
+void AddUsers();
+void AddNewUser(); 
+UserData ReadNewUser();
+string ConvertUserRecordToLine(UserData User, string Separator);
+void UpdateUserScreen();
+bool UpdateUserByUserName(string UserName, vector <UserData>& vUsers);
+UserData ChangeUserRecord(string UserName);
+vector <UserData> SaveUsersDataToFile(string FileNameUser, vector <UserData> vUsers);
+void DeleteUsersScreen();
+bool MarkUserForDeleteByUserName(string UserName, vector <UserData>& vUsers);
+bool DeleteUserByAccountNumber(string UserName, vector <UserData>& vUsers);
+int ReadPermissionsToSet();
+void ShowAccessDeniedScreen();
+bool CheckAccessPermission(enPermissions Permission);
